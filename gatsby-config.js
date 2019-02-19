a7a3729547898fcb6ebba02d9080882188b37f0c
Lennart <lekoarts@gmail.com>
2019-02-19 21:48:34 +0100

    Add minimal config / content / packages

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+const config = require('./config')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
+module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-yaml',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'content',
+        path: `${__dirname}/content`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: config.googleAnalyticsID,
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitleShort,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'standalone',
+        icon: 'src/favicon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+  ],
+}
