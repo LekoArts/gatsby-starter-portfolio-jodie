777b6f8cc0f801266f1f3db0f13f24f02d84293d
LekoArts <lekoarts@gmail.com>
2019-02-24 22:04:47 +0100

    add heart icon and add overlay + animation

--- src/pages/instagram.jsx
+++ src/pages/instagram.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
-import {Flex} from 'rebass'
+import { Flex } from 'rebass'
 import styled from 'styled-components'
 import Layout from '../components/Layout'
+import Heart from '../heart.svg'
 
 const Grid = styled.div`
   display: grid;
@@ -18,14 +19,46 @@ const Grid = styled.div`
   }
 `
 
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 2;
+  opacity: 0;
+  transition: all 0.3s ease 0s;
+`
+
+const Bottom = styled(Flex)`
+  color: white;
+  @media (max-width: ${props => props.theme.breakpoints[2]}) {
+    font-size: ${props => props.theme.fontSizes[0]};
+  }
+  transform: translateY(45px);
+  opacity: 0;
+  transition: all 0.4s ease 0s;
+`
+
 const Item = styled.a`
   position: relative;
+  overflow: hidden;
   > div img {
     transition: all 0.3s ease 0s !important;
   }
   &:hover {
     > div img {
       transform: scale(1.05);
+    }
+    
+    ${Overlay} {
+      opacity: 1;
+    }
+    
+    ${Bottom} {
+      transform: translateY(0);
+      opacity: 1;
     }
   }
 `
@@ -40,13 +73,6 @@ const Content = styled(Flex)`
   padding: ${props => props.theme.space[5]};
 `
 
-const Bottom = styled(Flex)`
-  color: white;
-  @media (max-width: ${props => props.theme.breakpoints[2]}) {
-    font-size: ${props => props.theme.fontSizes[0]};
-  }
-`
-
 const Title = styled.div`
   color: white;
   font-weight: 700;
@@ -57,19 +83,26 @@ const Title = styled.div`
   }
 `
 
+const HeartIcon = styled.img`
+  width: 1.25rem;
+  height: 1.25rem;
+`
+
 const Instagram = ({ data: { instagram } }) => (
   <Layout>
     <Grid>
       {instagram.edges.map(({node: post}) => {
         // Grab everything before the first hashtag (because I write my captions like that)
         const title = post.caption.split('#')[0]
+        const date = new Date(post.timestamp * 1000).toLocaleDateString('de-DE')
 
         return (
           <Item href={`https://www.instagram.com/p/${post.id}/`} key={post.id}>
+            <Overlay />
             <Img fluid={post.localFile.childImageSharp.fluid} />
             <Content flexDirection="column" flexWrap="nowrap" justifyContent="space-between">
               <Title>{title}</Title>
-              <Bottom flexDirection="row" flexWrap="nowrap" justifyContent="space-between"><span>{post.likes}</span><span>{post.timestamp}</span></Bottom>
+              <Bottom flexDirection="row" flexWrap="nowrap" justifyContent="space-between"><span><HeartIcon src={Heart} /> {post.likes}</span><span>{date}</span></Bottom>
             </Content>
           </Item>
         )
