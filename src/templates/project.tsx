96b3024f1c918bbde0389a8b581a1045aaa6091c
Lennart <lekoarts@gmail.com>
2019-05-28 17:15:23 +0200

    style update

--- src/templates/project.tsx
+++ src/templates/project.tsx
@@ -125,7 +125,7 @@ const Project: React.FunctionComponent<PageProps> = ({ data: { project, images }
           ))}
         </PBox>
       </Content>
-      <PBox textAlign="center" py={10} px={[6, 6, 8, 10]}>
+      <PBox style={{ textAlign: 'center' }} py={10} px={[6, 6, 8, 10]}>
         <h2>Want to start your own project?</h2>
         <PButton color={project.color} py={4} px={8}>
           Contact Us
