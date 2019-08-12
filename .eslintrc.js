08b4cffe9a5d63397ed6283f655216255b6d6665
Lennart <lekoarts@gmail.com>
2019-08-12 14:15:44 +0200

    deactivate "react/jsx-props-no-spreading"

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,6 +68,7 @@ module.exports = {
     "react/forbid-prop-types": 0,
     "react/no-unescaped-entities": 0,
     "react/prop-types": 0,
+    "react/jsx-props-no-spreading": 0,
     "react/jsx-filename-extension": [
       1,
       {
