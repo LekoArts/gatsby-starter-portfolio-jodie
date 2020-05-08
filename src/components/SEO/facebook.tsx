f2b327c8e221be3e7c91101becc0b639aa3cfc5b
LekoArts <lekoarts@gmail.com>
2020-05-08 11:35:27 +0200

    update imports

--- src/components/SEO/facebook.tsx
+++ src/components/SEO/facebook.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 
 type Props = { url: string; title: string; desc: string; image: string; locale: string } & typeof defaultProps
 
