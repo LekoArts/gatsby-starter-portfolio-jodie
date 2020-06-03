a1f215d26b35a702252cd8dccbc32675f514557f
AadithyaVenkat <aadithyavenkat@gmail.com>
2020-06-03 10:47:19 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,6 @@ type PageProps = {
       }[]
     }
     aboutUs: ChildImageSharp
-    instagram: ChildImageSharp
   }
 }
 
@@ -33,8 +32,7 @@ const Area = styled(animated.div)`
   grid-template-rows: 35vw 40vw 25vw;
   grid-template-areas:
     'first-project about-us about-us'
-    'three-projects three-projects three-projects'
-    'instagram instagram instagram';
+    'three-projects three-projects three-projects';
 
   @media (max-width: ${(props) => props.theme.breakpoints[3]}) {
     grid-template-columns: repeat(4, 1fr);
@@ -43,8 +41,7 @@ const Area = styled(animated.div)`
     grid-template-areas:
       'first-project first-project about-us about-us'
       'three-projects three-projects three-projects three-projects'
-      'three-projects three-projects three-projects three-projects'
-      'instagram instagram instagram instagram';
+      'three-projects three-projects three-projects three-projects';
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[1]}) {
@@ -55,8 +52,7 @@ const Area = styled(animated.div)`
       'first-project about-us'
       'three-projects three-projects'
       'three-projects three-projects'
-      'three-projects three-projects'
-      'instagram instagram';
+      'three-projects three-projects';
   }
 
   @media (max-width: ${(props) => props.theme.breakpoints[0]}) {
@@ -68,8 +64,7 @@ const Area = styled(animated.div)`
       'about-us'
       'three-projects'
       'three-projects'
-      'three-projects'
-      'instagram';
+      'three-projects';
   }
 `
 
@@ -92,9 +87,6 @@ const ThreeProjects = styled.div`
   }
 `
 
-const Instagram = styled(GridItem)`
-  grid-area: instagram;
-`
 
 const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, threeProjects, aboutUs, instagram } }) => {
   const pageAnimation = useSpring({
@@ -123,10 +115,6 @@ const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, three
             </GridItem>
           ))}
         </ThreeProjects>
-        <Instagram to="/instagram" aria-label="See my Instagram pictures">
-          <Img fluid={instagram.childImageSharp.fluid} />
-          <span>Instagram</span>
-        </Instagram>
       </Area>
     </Layout>
   )
@@ -163,13 +151,6 @@ export const query = graphql`
     aboutUs: file(sourceInstanceName: { eq: "images" }, name: { eq: "about-us" }) {
       childImageSharp {
         fluid(quality: 95, maxWidth: 1200) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-    instagram: file(sourceInstanceName: { eq: "images" }, name: { eq: "instagram" }) {
-      childImageSharp {
-        fluid(quality: 95, maxWidth: 1920) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
