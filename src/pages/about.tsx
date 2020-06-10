235a70476775d8e98a90fddb4ce608bc4fcdaf2d
AadithyaVenkat <aadithyavenkat@gmail.com>
2020-06-09 18:12:52 -0700

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -13,12 +13,13 @@ const About = () => {
 
   return (
     <Layout>
-      <SEO title="About | Jodie" desc="Hi. I'm LekoArts! You can visit my website or my other Gatsby projects." />
+      <SEO title="About | Jodie" desc="Hi. I'm Aadithya! While I am not writing software, you can find me on a trail clicking pictures or playing some music or Tennis You can visit my Instagram or my Youtube Channel" />
       <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
-        <h1>Hi. I'm LekoArts!</h1>
+        <h1>Hi. I'm Aadithya!</h1>
         <p>
-          You can visit my <a href="https://www.lekoarts.de/en">website</a> or my other{' '}
-          <a href="https://gatsby-starter-portfolio.netlify.com">Gatsby projects</a>.
+          While I am not writing software, you can find me on a trail clicking pictures or playing some music or Tennis
+          You can visit my <a href="https://www.instagram.com/aadithyaphotography/">Instagram</a> or my {' '}
+          <a href="https://www.youtube.com/channel/UCACUH8-MUb4r_myA211REZg?view_as=subscriber">Youtube Channel</a>.
         </p>
       </AnimatedBox>
     </Layout>
